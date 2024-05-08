7445f30d6d6372d4cdc7ac02eda7067aabc88156
Anubhav Chaturvedi <chaturvedianubhav520@gmail.com>
2024-05-08 11:15:50 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NetHyTech-MCV",
+    version="1.0",
+    author="Anubhav Chaturvedi",
+    author_email="chaturvedianubhav520@gmail.com",
+    description="A package for controlling mouse actions using hand gestures",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AnubhavChaturvedi-GitHub/NetHyTech-MCV",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'opencv-python',
+        'mediapipe',
+        'pyautogui',
+        'numpy',
+        'pynput'
+    ]
+)
+
+
+'''
+python setup.py bdist_wheel 
+
+twine upload --repository-url https://upload.pypi.org/legacy/ dist/*  
+'''
